145a8326c8ee7e6501b5425ba351b7bf113aedc8
Felipe <felipepozo.dev@gmail.com>
2024-08-10 06:10:09 -0300

    update folders, html, css and create const obj pieces

---
+++ src/pieces/PIECES.js
@@ -0,0 +1,10 @@
+export const PIECES = {
+  king: {
+    black: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Chess_kdt45.svg/45px-Chess_kdt45.svg.png',
+    white: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Chess_klt45.svg/45px-Chess_klt45.svg.png'
+  },
+  queen: {
+    black: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Chess_qdt45.svg/45px-Chess_qdt45.svg.png',
+    white: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Chess_qlt45.svg/45px-Chess_qlt45.svg.png'
+  }
+}
