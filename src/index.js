b68b07c54c3f562fe2625cee1d4e63991176ddad
Felipe <felipepozo.dev@gmail.com>
2024-08-10 07:11:09 -0300

    render piece depend index and color

--- src/index.js
+++ src/index.js
@@ -1,16 +1,47 @@
 import { PIECES } from './pieces/PIECES.js'
+import { ORDER_PIECES } from './pieces/ORDER_PIECES.js'
 
 const TABLE = document.querySelectorAll('#table')
+const FILA = document.querySelectorAll('.fila')
+
+const TABLE_LENGHT = TABLE[0].children.length
 
 window.addEventListener('DOMContentLoaded', loadTable)
 
 function loadTable () {
-  for (let i = 0; i < TABLE.length; i++) {
-    console.log({ TABLE: TABLE[i].children })
-    const childrenDiv = TABLE[i].children
-
+  for (let i = 0; i < TABLE_LENGHT; i++) {
+    const fila = FILA[i]
     if (i === 0) {
-
+      ORDER_PIECES.up.pieces.forEach((piece, index) => {
+        const span = fila.children[index]
+        const iPiece = document.createElement('i')
+        iPiece.style.backgroundImage = `url(${PIECES[piece].black})`
+        span.appendChild(iPiece)
+      })
+    }
+    if (i === 1) {
+      ORDER_PIECES.up.pieces.forEach((_, index) => {
+        const span = fila.children[index]
+        const iPiece = document.createElement('i')
+        iPiece.style.backgroundImage = `url(${PIECES.pawn.black})`
+        span.appendChild(iPiece)
+      })
+    }
+    if (i === 6) {
+      ORDER_PIECES.down.pieces.forEach((_, index) => {
+        const span = fila.children[index]
+        const iPiece = document.createElement('i')
+        iPiece.style.backgroundImage = `url(${PIECES.pawn.white})`
+        span.appendChild(iPiece)
+      })
+    }
+    if (i === 7) {
+      ORDER_PIECES.down.pieces.forEach((piece, index) => {
+        const span = fila.children[index]
+        const iPiece = document.createElement('i')
+        iPiece.style.backgroundImage = `url(${PIECES[piece].white})`
+        span.appendChild(iPiece)
+      })
     }
   }
 }
---
+++ src/pieces/ORDER_PIECES.js
@@ -0,0 +1,10 @@
+export const ORDER_PIECES = {
+  up: {
+    pieces: ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook'],
+    pawn: ['pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn']
+  },
+  down: {
+    pawn: ['pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn'],
+    pieces: ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook']
+  }
+}
